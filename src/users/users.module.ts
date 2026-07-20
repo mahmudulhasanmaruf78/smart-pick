fec3bc4a4948d446da25a1869169277bfcadff4f
Hmanipulative9 <haass9b@gmail.com>
2026-07-20 21:48:33 +0600

    updated users_module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user.entity';
+import { RiderVerification } from './rider-verification.entity';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([User, RiderVerification]),
+  ],
+  controllers: [UsersController],
   providers: [UsersService],
-  controllers: [UsersController]
+  exports: [UsersService, TypeOrmModule],
 })
-export class UsersModule {}
+export class UsersModule { }
