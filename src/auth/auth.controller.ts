324841caf54b557495c582498733140b9e367b89
Hmanipulative9 <haass9b@gmail.com>
2026-07-23 22:20:13 +0600

    auth controller files imported

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import {
+    Controller,
+    Post,
+    Body,
+    UseInterceptors,
+    UploadedFile,
+    BadRequestException,
+} from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { RegisterCustomerDto } from './dto/register-customer.dto';
+import { RegisterRiderDto } from './dto/register-rider.dto';
+import { LoginDto } from './dto/login.dto';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { extname } from 'path';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController { }
