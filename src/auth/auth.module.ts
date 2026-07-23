3fdc00b90346d1ca723837c428bec24983f54d53
Hmanipulative9 <haass9b@gmail.com>
2026-07-23 22:07:34 +0600

    added module auth

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { UsersModule } from '../users/users.module';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
+  imports: [
+    UsersModule, // Imports UsersService so AuthService can use it
+    JwtModule.register({
+      global: true,
+      secret: 'hasnain', // Secret key for token encryption
+      signOptions: { expiresIn: '1d' }, // Token valid for 24 hours
+    }),
+  ],
+  controllers: [AuthController],
   providers: [AuthService],
-  controllers: [AuthController]
+  exports: [AuthService],
 })
-export class AuthModule {}
+export class AuthModule { }
