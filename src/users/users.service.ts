779a343a6e491d0533f454486aaaaf42dd592fa4
Hmanipulative9 <haass9b@gmail.com>
2026-07-23 21:44:30 +0600

    fix issue

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -15,9 +15,7 @@ export class UsersService {
 
     async findByIdentity(identity: string): Promise<User | null> {
         return this.userRepository.findOne({
-            where: [
-                { email: identity },
-                { phone: identity },
+            
             ]
         })
     }
