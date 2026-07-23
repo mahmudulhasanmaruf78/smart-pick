ce5a4dac0a01ba88d7da6fe555b5832531794d7a
Hmanipulative9 <haass9b@gmail.com>
2026-07-23 21:04:55 +0600

    imported files

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
+import { RiderVerification } from './rider-verification.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    constructor(
+        @InjectRepository(User)
+        private readonly userRepository: Repository<User>,
+    ) { }
+}
