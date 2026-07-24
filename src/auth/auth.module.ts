c1c97401f4d8a24601a75dba7c6fae7482ac92f1
Hmanipulative9 <haass9b@gmail.com>
2026-07-24 20:35:56 +0600

    imported files updated

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,11 +3,14 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UsersModule } from '../users/users.module';
 import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtStrategy } from './jwt.strategy';
+import { RolesGuard } from './roles.guard';
 
 @Module({
   imports: [
     UsersModule, // Imports UsersService so AuthService can use it
-    JwtModule.register({
+    JwtModule.registerAsync({
       global: true,
       secret: 'hasnain', // Secret key for token encryption
       signOptions: { expiresIn: '1d' }, // Token valid for 24 hours
