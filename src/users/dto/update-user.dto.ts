f5e8ec12a52977edd22bda86291ac2df1dc76690
mahmudulhasanmaruf78@gmail.com <mahmudulhasanmaruf78@gmail.com>
2026-07-24 23:47:52 +0600

    create UpdateUserDto for user profile modifications

---
+++ src/users/dto/update-user.dto.ts
@@ -0,0 +1,18 @@
+import { IsOptional, IsString, MinLength } from "class-validator";
+
+export class UpdateUserDto {
+    @IsOptional()
+    @IsString()
+    @MinLength(3)
+    name: string;
+
+    @IsOptional()
+    @IsString()
+    @MinLength(6)
+    password: string;
+
+    @IsOptional()
+    @IsString()
+    phone: string;
+
+}
