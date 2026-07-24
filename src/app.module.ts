51360f8b1c58ad7421b47823f7ef0ea2485716b4
mahmudulhasanmaruf78@gmail.com <mahmudulhasanmaruf78@gmail.com>
2026-07-24 15:13:07 +0600

    Import ConfigModule and TypeOrmModule in AppModule for PostgreSQL configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ZonesModule } from './zones/zones.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [ZonesModule],
