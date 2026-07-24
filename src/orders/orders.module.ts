b1f1107aae0d597688f4c91bf8ade1feafb22df6
professor4044 <mejamirsheikh@gmail.com>
2026-07-24 19:04:16 +0600

    update orders module with typeorm imports and exports

--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { OrdersController } from './orders.controller';
 import { OrdersService } from './orders.service';
+import { Order } from './entities/order.entity';
+import { DeliveryZone } from '../zones/entities/delivery-zone.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Order, DeliveryZone])],
   controllers: [OrdersController],
-  providers: [OrdersService]
+  providers: [OrdersService],
+  exports: [OrdersService],
 })
-export class OrdersModule {}
+export class OrdersModule { }
