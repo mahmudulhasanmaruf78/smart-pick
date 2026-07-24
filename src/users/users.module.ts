fbc2625eade9bdbff8a047b1f0aebed7d979e8ab
mahmudulhasanmaruf78@gmail.com <mahmudulhasanmaruf78@gmail.com>
2026-07-24 23:41:17 +0600

    error fix

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,13 +2,10 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { RiderVerification } from './entities/rider-verification.entity';
-import { UsersController } from './users.controller';
-import { UsersService } from './users.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User, RiderVerification])],
   exports: [TypeOrmModule],
-  controllers: [UsersController],
-  providers: [UsersService],
 })
-export class UsersModule {}
+export class UsersModule { }
+
