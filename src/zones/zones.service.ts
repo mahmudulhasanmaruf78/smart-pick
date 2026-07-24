aa01ea01993f8073d134f12372e2fcf0c7b83633
mahmudulhasanmaruf78@gmail.com <mahmudulhasanmaruf78@gmail.com>
2026-07-24 15:36:26 +0600

    fix error

--- src/zones/zones.service.ts
+++ src/zones/zones.service.ts
@@ -1,4 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { DeliveryZone } from './entities/delivery-zone.entity';
 
 @Injectable()
-export class ZonesService {}
+export class ZonesService implements OnModuleInit {
+  constructor(
+    @InjectRepository(DeliveryZone)
+    private readonly zoneRepo: Repository<DeliveryZone>,
+  ) {}
+  onModuleInit() {
+    console.log('ZonesService initialized, repo:', !!this.zoneRepo);
+  }
+}
