e29d50bc47daa27e994b496e41f6589b6c4bd7ae
mahmudulhasanmaruf78@gmail.com <mahmudulhasanmaruf78@gmail.com>
2026-07-24 14:33:40 +0600

    Update TypeORM and adjust dependencies in package.json and package-lock.json; enhance ZonesModule with TypeOrmModule integration for DeliveryZone entity.

--- src/zones/zones.module.ts
+++ src/zones/zones.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ZonesController } from './zones.controller';
 import { ZonesService } from './zones.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DeliveryZone } from './entities/delivery-zone.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([DeliveryZone])],
   controllers: [ZonesController],
-  providers: [ZonesService]
+  providers: [ZonesService],
+  exports: [ZonesService],
 })
 export class ZonesModule {}
