e81e9ecfd8fc7480bd76d0ecee9e4355cd9748ad
Hmanipulative9 <haass9b@gmail.com>
2026-07-24 20:31:22 +0600

    rolesguard

--- src/auth/roles.guard.ts
+++ src/auth/roles.guard.ts
@@ -4,3 +4,9 @@ import { Reflector } from "@nestjs/core";
 @Injectable()
 export class RolesGuard implements CanActivate {
     constructor(private readonly reflector: Reflector) { }
+    canActivate(context: ExecutionContext): boolean {
+        // Read the required roles attached by @roles(...) decorator
+        const requiredRoles = this.reflector.getAllAndOverride<string[]>('roles', [
+            context.getHandler(),
+            context.getClass(),
+        ]);
