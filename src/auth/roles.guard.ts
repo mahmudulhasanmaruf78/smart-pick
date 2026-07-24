b497fe9716cd8e9b684e635c1be2700a2fdcba15
Hmanipulative9 <haass9b@gmail.com>
2026-07-24 20:30:34 +0600

    rolesguard class added

--- src/auth/roles.guard.ts
+++ src/auth/roles.guard.ts
@@ -1,3 +1,6 @@
 import { CanActivate, ExecutionContext, Injectable, ForbiddenException } from "@nestjs/common";
 import { Reflector } from "@nestjs/core";
 
+@Injectable()
+export class RolesGuard implements CanActivate {
+    constructor(private readonly reflector: Reflector) { }
