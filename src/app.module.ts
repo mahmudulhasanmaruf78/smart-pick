8d72fb1859751566468e16902df121dfb5cb8a98
mahmudulhasanmaruf78@gmail.com <mahmudulhasanmaruf78@gmail.com>
2026-07-24 15:19:18 +0600

    Configure PostgreSQL connection in AppModule using TypeOrmModule and ConfigModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,7 +6,24 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ZonesModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: configService.get('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: true,
+      }),
+    }),
+    ZonesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
