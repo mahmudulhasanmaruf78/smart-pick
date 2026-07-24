d62cdb3426cb9ba436b040234bb9d9e2bdfce39c
mahmudulhasanmaruf78@gmail.com <mahmudulhasanmaruf78@gmail.com>
2026-07-24 23:37:44 +0600

    initialize users module with controller, service, and data transfer objects

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { RiderVerification } from './entities/rider-verification.entity';
+import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User, RiderVerification])],
   exports: [TypeOrmModule],
+  controllers: [UsersController],
+  providers: [UsersService],
 })
 export class UsersModule {}
