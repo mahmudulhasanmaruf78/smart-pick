6d3c5814df1c5b0f1089c1d389b3f2df62a20770
Hmanipulative9 <haass9b@gmail.com>
2026-07-24 20:26:03 +0600

    secret key added

---
+++ src/auth/jwt.strategy.ts
@@ -0,0 +1,5 @@
+import { Injectable } from "@nestjs/common";
+import { PassportStrategy } from "@nestjs/passport";
+import { ExtractJwt, Strategy } from "passport-jwt";
+import { ConfigService } from "@nestjs/config";
+
