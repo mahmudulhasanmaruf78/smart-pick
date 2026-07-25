a36702655ad0fe68f31b1c81bdcfce4958ce804d
mahmudulhasanmaruf78@gmail.com <mahmudulhasanmaruf78@gmail.com>
2026-07-25 13:03:37 +0600

    initialize AdminModule with TypeORM entities and controller setup

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
-import { AdminController } from './admin.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AdminService } from './admin.service';
+import { AdminController } from './admin.controller';
+import { User } from '../users/entities/user.entity';
+import { Order } from '../orders/entities/order.entity';
+import { RiderVerification } from '../users/entities/rider-verification.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([User, RiderVerification, Order])],
   controllers: [AdminController],
-  providers: [AdminService]
+  providers: [AdminService],
 })
 export class AdminModule {}
